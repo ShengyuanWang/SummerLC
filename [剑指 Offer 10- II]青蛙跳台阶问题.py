4f3931cd246319da847826b35166bbf6ec31e862
ShengyuanWang <78770936+ShengyuanWang@users.noreply.github.com>
2022-05-03 15:48:57 -0500

    Update [剑指 Offer 10- II]青蛙跳台阶问题.py

--- [剑指 Offer 10- II]青蛙跳台阶问题.py
+++ [剑指 Offer 10- II]青蛙跳台阶问题.py
@@ -34,4 +34,8 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def numWays(self, n: int) -> int:
+        a, b = 0, 1
+        for _ in range(n):
+            a, b = b, a+b
+        return b % 1000000007
 # leetcode submit region end(Prohibit modification and deletion)
